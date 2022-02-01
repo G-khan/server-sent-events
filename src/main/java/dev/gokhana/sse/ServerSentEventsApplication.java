0d6ed605b31bf431c9ac31d362fe503e75eff948
Gökhan Ayrancıoğlu <gkhan.a.06@gmail.com>
2022-02-01 17:25:31 +0300

    first commit

---
+++ src/main/java/dev/gokhana/sse/ServerSentEventsApplication.java
@@ -0,0 +1,13 @@
+package dev.gokhana.sse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerSentEventsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerSentEventsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/gokhana/sse/ServerSentEventsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.gokhana.sse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerSentEventsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
